3e64cc86b278126d5cc080933b45c09f56560ce9
Abhishek Anand <76440541+Abhishek-Anand21@users.noreply.github.com>
2022-06-06 22:31:26 +0530

    Code

---
+++ ValidParentheses.cpp
@@ -0,0 +1,26 @@
+stack<char> st;
+    for(int i=0;i<s.length();i++) {
+        if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+            st.push(s[i]);
+        else if(s[i] == ')') {
+            if(st.empty() == 0 && st.top() == '(' )
+                st.pop();
+            else
+                return false;
+        }
+        else if(s[i] == ']') {
+            if(st.empty() == 0 && st.top() == '[')
+                st.pop();
+            else
+                return false;
+        }
+        else if(s[i] == '}') {
+            if(st.empty() == 0 && st.top() == '{')
+                st.pop();
+            else
+                return false;
+        }
+    }
+    if(st.empty())
+        return true;
+    return false;
