9587b06cb49c6f35fc9945eea7acdfb0e1b5cacc
Abhishek Anand <76440541+Abhishek-Anand21@users.noreply.github.com>
2022-06-06 22:32:00 +0530

    Code

--- ValidParentheses.cpp
+++ ValidParentheses.cpp
@@ -1,4 +1,6 @@
-stack<char> st;
+bool valid(string s)
+{
+    stack<char> st;
     for(int i=0;i<s.length();i++) {
         if(s[i]=='(' || s[i]=='[' || s[i]=='{')
             st.push(s[i]);
@@ -24,3 +26,4 @@ stack<char> st;
     if(st.empty())
         return true;
     return false;
+}
